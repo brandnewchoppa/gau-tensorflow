f0c0be1fcd2cf34d3b30a91094678e1c7a57d3f3
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2023-09-15 15:16:32 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'gau-tensorflow',
+    packages = find_packages(exclude = []),
+    version = '0.0.9',
+    license = 'MIT',
+    description = 'GAU - Transformer Quality in Linear Time - TensorFlow',
+    author = 'brandnewchoppa',
+    author_email = '',
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/brandnewchoppa/gau-tensorflow',
+    keywords = [
+        'artificial intelligence',
+        'deep learning',
+        'transformers',
+        'attention mechanism',
+        'gated attention unit'
+    ],
+    install_requires = [
+        'tensorflow>=2.13.0'
+    ],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.11'
+    ]
+)
