e8623c0e148d0d96166e3793ab9a889ebfb77d86
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2023-09-21 09:35:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'gau-tensorflow',
     packages = find_packages(exclude = []),
-    version = '0.1.0',
+    version = '0.1.3',
     license = 'MIT',
     description = 'GAU - Transformer Quality in Linear Time - TensorFlow',
     author = 'brandnewchoppa',
